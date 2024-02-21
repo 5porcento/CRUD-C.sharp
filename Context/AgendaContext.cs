711a8f17e1a3393ab3469a72da1f6c2cda5075a9
Kauan Pacheco <kauanpacheco.dev@gmail.com>
2024-02-20 23:05:40 -0300

    Adicionar arquivos de projeto.

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Entities;
+namespace WebApplication2.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext>options) : base(options)
+        { }  
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
---
+++ Entities/Contato.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication2.Entities
+{
+    public class Contato
+    {
+        public int Id{ get; set; }
+        public string Nome { get; set; }
+
+        public string Telefone { get; set;}
+
+        public bool Ativo { get; set;}
+
+        
+    }
+}
